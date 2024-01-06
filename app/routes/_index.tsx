48d78456beda4d1d93d790e28c9e999e3a6a0e30
Amadeus Mader <amadeus@mozart409.com>
2024-01-06 20:45:05 +0100

    feat: :tada: init

---
+++ app/routes/_index.tsx
@@ -0,0 +1,18 @@
+import type { MetaFunction } from "@remix-run/node";
+import { Button } from "~/components/ui/button";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Hello World</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
