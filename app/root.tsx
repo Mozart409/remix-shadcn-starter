5f0ef1e0722ff5b55df95322f018aa0bb80f15e2
Amadeus Mader <amadeus@mozart409.com>
2024-01-20 23:15:01 +0100

    Add analytics tracking script

--- app/root.tsx
+++ app/root.tsx
@@ -4,12 +4,17 @@ import { PreventFlashOnWrongTheme, ThemeProvider, useTheme } from "remix-themes"
 
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from "@remix-run/react";
-import "./tailwind.css";
-import "remix-development-tools/index.css";
+import invariant from "invariant";
 import { Link, XCircle } from "lucide-react";
+import "remix-development-tools/index.css";
 import { Button } from "./components/ui/button";
 import { Toaster } from "./components/ui/toaster";
 import { themeSessionResolver } from "./sessions.server";
+import "./tailwind.css";
+
+invariant(process.env.ENABLE_ANALYTICS, "Missing ENABLE_ANALYTICS env variable");
+invariant(process.env.ANALYTICS_URL, "Missing ANALYTICS_URL env variable");
+invariant(process.env.ANALYTICS_ID, "Missing ANALYTICS_ID env variable");
 // Return the theme from the session storage using the loader
 export async function loader({ request }: LoaderFunctionArgs) {
   const { getTheme } = await themeSessionResolver(request);
@@ -48,6 +53,10 @@ function App() {
         <Scripts />
         <Toaster />
         <LiveReload />
+
+        {process.env.NODE_ENV === "production" && process.env.ENABLE_ANALYTICS === "1" ? (
+          <script async src={`${process.env.ANALYTICS_URL}`} data-website-id={`${process.env.ANALYTICS_ID}`} />
+        ) : null}
       </body>
     </html>
   );
