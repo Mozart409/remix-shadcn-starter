eeec953424d05dfe959a449a42a53de399576aaa
Amadeus <38767929+Mozart409@users.noreply.github.com>
2024-10-28 22:12:52 +0100

    feat: :arrow_up: Refactor Vite configuration and dependencies
    This commit refactors the Vite configuration in `vite.config.ts` by updating the imports for the `@remix-run/dev` package and removing the import for `remix-development-tools/vite`. Additionally, the dependencies in `package.json` have been updated, including the versions of `isbot` and `lucide-react`. The `remix-development-tools` package has been updated to version 4.7.3 and the `vite-tsconfig-paths` package has been updated to version 5.0.1. The unused import of `remix-development-tools/index.css` has been removed from `app/root.tsx`.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
-import { remixDevTools } from "remix-development-tools/vite";
+import { remixDevTools } from "remix-development-tools";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 installGlobals();
