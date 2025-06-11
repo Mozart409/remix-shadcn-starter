45d6b3ffcda6ea0d42509ad4670a48eb82f20fb8
Amadeus <amadeus@mozart409.com>
2025-06-11 17:43:08 +0200

    feat: add shell.nix for development environment setup

---
+++ shell.nix
@@ -0,0 +1,15 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.05";
+  pkgs = import nixpkgs {
+    config = { };
+    overlays = [ ];
+  };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    nodejs_20
+    pnpm
+    ni # use the right package manager
+  ];
+}
