b99fcc464d70c69af9ba3ea4062960f8aa798174
Amadeus Mader <amadeus@mozart409.com>
2025-07-18 22:10:01 +0200

    feat: :arrow_up: upgrade pkgs

--- shell.nix
+++ shell.nix
@@ -1,15 +1,20 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.05";
-  pkgs = import nixpkgs {
-    config = { };
-    overlays = [ ];
-  };
-in
-
-pkgs.mkShellNoCC {
-  packages = with pkgs; [
-    nodejs_20
-    pnpm
-    ni # use the right package manager
+{pkgs ? import <nixpkgs> {}}:
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.nodejs_20
+    pkgs.pnpm
+    pkgs.docker
+    pkgs.ni
+    pkgs.lefthook
+    pkgs.biome
   ];
+
+  shellHook = ''
+    export NODE_ENV=development
+    echo "Node version: $(node --version)"
+    echo "PNPM version: $(pnpm --version)"
+    echo "Docker version: $(docker --version)"
+    echo "Lefthook version: $(lefthook version)"
+    lefthook install
+  '';
 }
