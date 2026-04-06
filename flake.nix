d79ec2191da2186080edfdcd05ed44570da63d54
Amadeus <amadeus@Amadeuss-MacBook-Pro.local>
2026-04-06 22:52:35 +0200

    chore: upgrade setup

--- flake.nix
+++ flake.nix
@@ -6,13 +6,17 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {inherit system;};
     in {
       devShells.default = pkgs.mkShell {
         buildInputs = [
-          pkgs.nodejs_20
+          pkgs.nodejs_22
           pkgs.pnpm
           pkgs.docker
           pkgs.ni
